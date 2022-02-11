68446e7137c67e3cd66fddfb52c647d8a83b319f
tmcgirr <59525360+tmcgirr@users.noreply.github.com>
2022-02-10 23:52:39 -0800

    Update About.js
    Update About Page

--- src/About/About.js
+++ src/About/About.js
@@ -23,61 +23,24 @@ function About() {
             I troubleshoot database issues using SQL.
             <br />
             <br />
-            I've continued to advance my knowledge in JavaScript, React, Node, Express, and PostgreSQL
-            through various courses. The Codecademy Full-Stack Engineer
-            Path provided a solid foundation for developing applications from
-            the wireframing and planning, to writing the HTML and styling with
-            CSS, adding functionality with React and implementing a database with 
-            Express and PsotgreSQL. 
+            I've continued to advance my knowledge in JavaScript, React, Node,
+            Express, and PostgreSQL through various courses. The Codecademy
+            Full-Stack Engineer Path provided a solid foundation for developing
+            applications from the wireframing and planning, to writing the HTML
+            and styling with CSS, adding functionality with React and
+            implementing a database with Express and PsotgreSQL.
             <br />
             <br />
             Here are a few of the skills I've attained so far:
           </p>
-        </div>
-
-        {/* <!-- ABOUT Grid --> */}
-        <div className="container grid grid--center-v">
-          {/* <!-- STEP 01  (HTML/CSS)--> */}
-          <div className="step-text-box">
-           
-            <h3 className="heading-tertiary">HTML & CSS</h3>
-            
-          </div>
-
-          {/* <!-- STEP 02 (JavaScript)--> */}
-          <div className="step-text-box">
-            
-            <h3 className="heading-tertiary">JavaScript</h3>
-           
-          </div>
-
-          {/* <!-- STEP 03 (React) --> */}
-          <div className="step-text-box">
-          
-            <h3 className="heading-tertiary">React</h3>
-          
-          </div>
-
-          {/* <!-- STEP 04 (Node) --> */}
-          <div className="step-text-box">
-          
-            <h3 className="heading-tertiary">Node</h3>
-           
-          </div>
-
-          {/* <!-- STEP 05 (Express) --> */}
-          <div className="step-text-box">
-          
-            <h3 className="heading-tertiary">Express</h3>
-           
-          </div>
-
-          {/* <!-- STEP 06 (SQL) --> */}
-          <div className="step-text-box">
-          
-            <h3 className="heading-tertiary">SQL (PostGreSQL & MSSQL)</h3>
-           
-          </div>
+          <br />
+          <br />
+          <h2 className="heading-tertiary">HTML & CSS</h2>
+          <h2 className="heading-tertiary">JavaScript</h2>
+          <h2 className="heading-tertiary">React</h2>
+          <h2 className="heading-tertiary">Node</h2>
+          <h2 className="heading-tertiary">Express</h2>
+          <h2 className="heading-tertiary">SQL (PostgreSQL & MSSQL)</h2>
         </div>
       </section>
     </>

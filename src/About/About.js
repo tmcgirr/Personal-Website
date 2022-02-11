fcce77921ee69706971b9a958fb93732fe79d58d
tmcgirr <59525360+tmcgirr@users.noreply.github.com>
2022-02-10 23:44:57 -0800

    Update About.js
    Update About Page

--- src/About/About.js
+++ src/About/About.js
@@ -23,13 +23,12 @@ function About() {
             I troubleshoot database issues using SQL.
             <br />
             <br />
-            I've continued to advance my knowledge in HTML, CSS, JavaScript and
-            React through various courses. The Codecademy Front-End Engineer
+            I've continued to advance my knowledge in JavaScript, React, Node, Express, and PostgreSQL
+            through various courses. The Codecademy Full-Stack Engineer
             Path provided a solid foundation for developing applications from
             the wireframing and planning, to writing the HTML and styling with
-            CSS, and adding functionality with JavaScript and React. I continued
-            to learn and expand my knowledge with various tutorials on SQL
-            database foundations.
+            CSS, adding functionality with React and implementing a database with 
+            Express and PsotgreSQL. 
             <br />
             <br />
             Here are a few of the skills I've attained so far:
@@ -40,53 +39,44 @@ function About() {
         <div className="container grid grid--center-v">
           {/* <!-- STEP 01  (HTML/CSS)--> */}
           <div className="step-text-box">
-            <p className="step-number">01</p>
+           
             <h3 className="heading-tertiary">HTML & CSS</h3>
-            <p className="step-description">
-              I created this website from scratch using the latest elements of
-              HTML and CSS. Combining the methodology from various programs and
-              tutorials several components are built using Flexbox, CSS Grid,
-              and Color Theory.
-            </p>
+            
           </div>
 
           {/* <!-- STEP 02 (JavaScript)--> */}
           <div className="step-text-box">
-            <p className="step-number">02</p>
+            
             <h3 className="heading-tertiary">JavaScript</h3>
-            <p className="step-description">
-              I've implemented various modern JavaScript (ES6+) features to make
-              this site responsive and interactive. The buttons and forms
-              written in this site allow for a user to interact and submit
-              responses. Many of the Projects use APIs, such as the Weather
-              component, use the OpenWeather API to provide data for a query or
-              geolocation input. The Google calendar uses the Google API and
-              authentication to provide the users upcoming events.
-            </p>
+           
           </div>
 
           {/* <!-- STEP 03 (React) --> */}
           <div className="step-text-box">
-            <p className="step-number">03</p>
+          
             <h3 className="heading-tertiary">React</h3>
-            <p className="step-description">
-              This site uses React Router to direct the user to each page
-              selected from the navigation menu. Each component in the Projects
-              section is built using React to allow for seamless rendering when
-              a user performs an action, such as location request or Google
-              account access. With the use of React hooks each component state
-              can be updated based on the requested data from the API call.
-            </p>
+          
           </div>
 
-          {/* <!-- STEP 04 (SQL) --> */}
+          {/* <!-- STEP 04 (Node) --> */}
           <div className="step-text-box">
-            <p className="step-number">04</p>
-            <h3 className="heading-tertiary">SQL</h3>
-            <p className="step-description">
-              My current knowledge of SQL Server and T-SQL foundations include
-              constructing, updating, and selecting data from the database.
-            </p>
+          
+            <h3 className="heading-tertiary">Node</h3>
+           
+          </div>
+
+          {/* <!-- STEP 05 (Express) --> */}
+          <div className="step-text-box">
+          
+            <h3 className="heading-tertiary">Express</h3>
+           
+          </div>
+
+          {/* <!-- STEP 06 (SQL) --> */}
+          <div className="step-text-box">
+          
+            <h3 className="heading-tertiary">SQL (PostGreSQL & MSSQL)</h3>
+           
           </div>
         </div>
       </section>
